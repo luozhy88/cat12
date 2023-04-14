492d27640fa32024f48e433a2941ac930f699b8b
luozhy88 <94740863+luozhy88@users.noreply.github.com>
2023-04-14 17:27:52 +0800

    Create run_bash.sh

---
+++ run_bash.sh
@@ -0,0 +1,35 @@
+
+# 使用cat_standalone.sh进行流程化
+
+cwd=/data/zhiyu/data/software/cat12_latest_R2017b_MCR_Linux/CAT12.8.2_R2017b_MCR_Linux
+cat12_dir=$cwd/spm12_mcr/home/gaser/gaser/spm/spm12/toolbox/cat12
+
+input_files="/data/fansih/20210814_MRI_train/sampledata/ADgroup/T1/subject*/*dcm"
+##Converted nifti
+
+${cwd}/standalone/cat_standalone.sh -s $cwd -m $cwd/v93    -b ${cwd}/standalone/cat_standalone_dicom2nii.m  $input_files  -a1 " 'patid' " -a2 "{'converted'}" -e
+
+##Segmentation
+
+t=0;
+for i in `ls converted/*/*/*nii`
+do
+((t++))
+echo $t
+echo "  
+cwd=$cwd
+cat12_dir=$cat12_dir
+
+\${cwd}/standalone/cat_standalone.sh  -s \$cwd -m \$cwd/v93 -b \${cwd}/standalone/cat_standalone_segment.m   -a1 \" '${cat12_dir}/templates_MNI152NLin2009cAsym/TPM_Age11.5.nii' \"   -a2 \" '${cat12_dir}/templates_MNI152NLin2009cAsym/Template_0_GS.nii' \"  ${i}    " >sh$t.sh;
+done
+
+ls sh* |parallel -j 60 bash -x 
+
+
+#${cwd}/standalone/cat_standalone.sh  -s $cwd -m $cwd/v93 -b ${cwd}/standalone/cat_standalone_segment.m   -a1 " '${cat12_dir}/templates_MNI152NLin2009cAsym/TPM_Age11.5.nii' "   -a2 " '${cat12_dir}/templates_MNI152NLin2009cAsym/Template_0_GS.nii' "  converted/*/*/*nii 
+
+##Smooth
+
+mkdir mwp1
+cp converted/*/*/mri/mwp1* mwp1
+${cwd}/standalone/cat_standalone.sh -m $cwd/v93 -b ${cwd}/standalone/cat_standalone_smooth.m  mwp1/*mwp1* -a1 "[6 6 6]" -a2 " 's6' "
